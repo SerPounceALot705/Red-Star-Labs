0e5f1306dc73e3f0bc5585c985449e4660922531
Valerie Dmitryuk <cosmic.pony705@gmail.com>
2020-07-11 18:46:36 +0800

    Сверстана страница "Оптовым клиентам"

--- src/pages/WholesaleCustomers.js
+++ src/pages/WholesaleCustomers.js
@@ -9,11 +9,20 @@ import "../components/wholesaleCustomers/css/wholesale-customers__main.css";
 import "../components/wholesaleCustomers/css/for-wholesale-customers.css";
 import "../components/wholesaleCustomers/css/for-wholesale-customers-form.css";
 import "../components/wholesaleCustomers/css/for-wholesale-customers-form.css";
+import "../components/wholesaleCustomers/css/for-wholesale-customers-form__text-container.css";
 import "../components/wholesaleCustomers/css/for-wholesale-customers-form__title.css";
 import "../components/wholesaleCustomers/css/for-wholesale-customers-form__subtitle.css";
 import "../components/wholesaleCustomers/css/for-wholesale-customers-form__text.css";
 import "../components/wholesaleCustomers/css/for-wholesale-customers-form__input-container.css";
 import "../components/wholesaleCustomers/css/for-wholesale-customers-form__input.css";
+import "../components/wholesaleCustomers/css/for-wholesale-customers-form__button.css";
+import "../components/wholesaleCustomers/css/for-wholesale-customers-advantages__container.css";
+import "../components/wholesaleCustomers/css/for-wholesale-customers-advantages__button.css";
+import "../components/wholesaleCustomers/css/for-wholesale-customers-advantages__buttons-container.css";
+import "../components/wholesaleCustomers/css/for-wholesale-customers-advantages__text.css";
+import "../components/wholesaleCustomers/css/for-wholesale-customers-advantages__title.css";
+import "../components/wholesaleCustomers/css/advantages__foto.css";
+import "../components/wholesaleCustomers/css/buttons__foto.css";
 
 
 class WholesaleCustomers extends Component {
@@ -28,12 +37,14 @@ class WholesaleCustomers extends Component {
                     <Header />
                     <div className="for-wholesale-customers">
                         <div className="for-wholesale-customers-form">
-                            <h2 className="for-wholesale-customers-form__title">Приглашаем к партнерству оптовых дистрибьюторов</h2>
-                            <h3 className="for-wholesale-customers-form__subtitle">Оставьте заявку, заполните поля ниже</h3>
-                            <p className="for-wholesale-customers-form__text">
-                                <span className="star">* </span>
+                            <div className="for-wholesale-customers-form__text-container">
+                                <h2 className="for-wholesale-customers-form__title">Приглашаем к партнерству оптовых дистрибьюторов</h2>
+                                <h3 className="for-wholesale-customers-form__subtitle">Оставьте заявку, заполните поля ниже</h3>
+                                <p className="for-wholesale-customers-form__text">
+                                    <span className="star">* </span>
                             - поля, обязательнные для заполнения
                             </p>
+                            </div>
                             <form className="for-wholesale-customers-form__input-container">
                                 <input className="for-wholesale-customers-form__input" placeholder="* Имя" type="text" minLength="2" maxLength="30" required></input>
                                 <input className="for-wholesale-customers-form__input" placeholder="* Email" type="email" minLength="2" maxLength="30" required></input>
@@ -43,26 +54,45 @@ class WholesaleCustomers extends Component {
                             </form>
                         </div>
                         <div className="for-wholesale-customers-advantages">
-                            <h2 className="for-wholesale-customers-advantages__title">Наши преимущества</h2>
+                            <h2 className="for-wholesale-customers-advantages__title">Наши преимущества:</h2>
                             <div className="for-wholesale-customers-advantages__container">
                                 <Image
-                                    src={require("../images/advantages.png").default}
-                                    alt={"advantages__foto"}
+                                    src={require("../images/advantages1.png").default}
+                                    alt={"advantages__foto01"}
                                     className={"advantages__foto"}
                                 />
-                                <div className="for-wholesale-customers-advantages__text-container">
-                                    <p className="for-wholesale-customers-advantages__text">Более 10 лет на рынке</p>
-                                    <p className="for-wholesale-customers-advantages__text">Индивидуальный подход к каждому клиенту</p>
-                                    <p className="for-wholesale-customers-advantages__text">Наличие товара на складе в России</p>
-                                    <p className="for-wholesale-customers-advantages__text">Высокая частота повторных покупок</p>
-                                </div>
+                                <p className="for-wholesale-customers-advantages__text">Более 10 лет на рынке</p>
+                            </div>
+                            <div className="for-wholesale-customers-advantages__container">
+                                <Image
+                                    src={require("../images/advantages2.png").default}
+                                    alt={"advantages__foto02"}
+                                    className={"advantages__foto"}
+                                />
+                                <p className="for-wholesale-customers-advantages__text">Индивидуальный подход к каждому клиенту</p>
+                            </div>
+                            <div className="for-wholesale-customers-advantages__container">
+                                <Image
+                                    src={require("../images/advantages3.png").default}
+                                    alt={"advantages__foto03"}
+                                    className={"advantages__foto"}
+                                />
+                                <p className="for-wholesale-customers-advantages__text">Наличие товара на складе в России</p>
+                            </div>
+                            <div className="for-wholesale-customers-advantages__container">
+                                <Image
+                                    src={require("../images/advantages4.png").default}
+                                    alt={"advantages__foto04"}
+                                    className={"advantages__foto"}
+                                />
+                                <p className="for-wholesale-customers-advantages__text">Высокая частота повторных покупок</p>
                             </div>
                             <div className="for-wholesale-customers-advantages__buttons-container">
                                 <div className="for-wholesale-customers-advantages__button-container">
                                     <Image
                                         src={require("../images/icon1.png").default}
                                         alt={"buttons__foto-01"}
-                                        className={"buttons__foto-01"}
+                                        className={"buttons__foto"}
                                     />
                                     <button className="for-wholesale-customers-advantages__button" type="submit" name="submit">Запросить прайс</button>
                                 </div>
@@ -70,7 +100,7 @@ class WholesaleCustomers extends Component {
                                     <Image
                                         src={require("../images/icon2.png").default}
                                         alt={"buttons__foto-02"}
-                                        className={"buttons__foto-02"}
+                                        className={"buttons__foto"}
                                     />
                                     <button className="for-wholesale-customers-advantages__button" type="submit" name="submit">Скачать каталог</button>
                                 </div>
