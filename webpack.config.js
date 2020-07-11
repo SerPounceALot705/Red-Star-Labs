655f2f4f41e8f495f842dce257b541e3becc34e5
tranee23 <tranee95@gmail.com>
2020-07-11 18:56:55 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     path: path.join(__dirname, "/dist"),
     filename: "index-bundle.js"
   },
+  devServer: {
+    historyApiFallback: true
+  },
   module: {
     rules: [
       {
