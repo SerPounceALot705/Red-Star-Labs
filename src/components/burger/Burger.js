b3909a6fc5322137660b232600e7207489c59779
Valerie Dmitryuk <cosmic.pony705@gmail.com>
2020-07-17 18:06:40 +0800

    Auto stash before merge of "master" and "origin/master"

--- src/components/burger/Burger.js
+++ src/components/burger/Burger.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { slide as Menu } from 'react-burger-menu';
+import LinkUrl from "../header/js/linkUrl.js";
+import LineCard from "../dropDownMenu/js/LineCard.js";
 
 import "./css/burger.css";
 
@@ -26,12 +28,110 @@ class Burger extends Component {
             <Menu right>
                 <div>
                     <ul>
-                        <li>123123</li>
+                        <li>
+                            <LinkUrl url={"/"} name={"Протеины"} className={"header__dropdown-content-title-link"} />
+                            <ul className="header__dropdown-content-list">
+                                <LinkUrl url={"/"} name={"Сывороточный"} className={"header__dropdown-content-link"} />
+                                <LinkUrl url={"/"} name={"Изолят"} className={"header__dropdown-content-link"} />
+                                <LinkUrl url={"/"} name={"Казеин"} className={"header__dropdown-content-link"} />
+                                <LinkUrl url={"/"} name={"Говяжий"} className={"header__dropdown-content-link"} />
+                            </ul>
+                        </li>
+                        <li><LinkUrl url={"/"} name={"Витамины и минералы"} className={"header__dropdown-content-title-link"} /></li>
+                        <li><LinkUrl url={"/"} name={"Аминокислоты и ВСАА"} className={"header__dropdown-content-title-link"} /></li>
+                        <li><LinkUrl url={"/"} name={"Карнитин"} className={"header__dropdown-content-title-link"} /></li>
+                        <li><LinkUrl url={"/"} name={"Специальные препараты"} className={"header__dropdown-content-title-link"} /></li>
+                        <li><LinkUrl url={"/"} name={"Батончики"} className={"header__dropdown-content-title-link"} /></li>
+                        <li><LinkUrl url={"/"} name={"Предтренировочные комплексы"} className={"header__dropdown-content-title-link"} /></li>
+                        <li><LinkUrl url={"/"} name={"Препараты для связок и суставов"} className={"header__dropdown-content-title-link"} /></li>
                     </ul>
                 </div>
-                <Link className="menu-item" to="/">Home</Link>
-                <Link className="menu-item" to="/about">About</Link>
-                <Link className="menu-item" to="/contact">Contact</Link>
+                <div className="header__dropdown-content-product">
+                    <h2 className="header__dropdown-content-title">Линейки</h2>
+                    <div className="header__dropdown-content-product-card">
+                        <LineCard
+                            className={"header__dropdown-content-card"}
+                            content={
+                                {
+                                    link: {
+                                        text: "Для насыщения организма витаминами",
+                                        url: "Card"
+                                    },
+                                    logo: {
+                                        url: require("../../images/logobasic.png").default,
+                                        alt: "logobasic"
+                                    },
+                                    product: {
+                                        url: require("../../images/whey.png").default,
+                                        alt: "whey"
+                                    }
+                                }
+                            } />
+                        <LineCard
+                            className={"header__dropdown-content-card"}
+                            content={
+                                {
+                                    link: {
+                                        text: "Для насыщения организма витаминами",
+                                        url: "Card"
+                                    },
+                                    logo: {
+                                        url: require("../../images/logoEssential.png").default,
+                                        alt: "logobasic"
+                                    },
+                                    product: {
+                                        url: require("../../images/Essentialbank.png").default,
+                                        alt: "whey"
+                                    }
+                                }
+                            } />
+                    </div>
+                    <div className="header__dropdown-content-product-card">
+                        <LineCard
+                            className={"header__dropdown-content-card"}
+                            content={
+                                {
+                                    link: {
+                                        text: "Для насыщения организма витаминами",
+                                        url: "Card"
+                                    },
+                                    logo: {
+                                        url: require("../../images/logoEssential.png").default,
+                                        alt: "logobasic"
+                                    },
+                                    product: {
+                                        url: require("../../images/premiumwhey.png").default,
+                                        alt: "whey"
+                                    }
+                                }
+                            } />
+                        <LineCard
+                            className={"header__dropdown-content-card"}
+                            content={
+                                {
+                                    link: {
+                                        text: "Для насыщения организма витаминами",
+                                        url: "Card"
+                                    },
+                                    logo: {
+                                        url: require("../../images/logoSuperior.png").default,
+                                        alt: "logobasic"
+                                    },
+                                    product: {
+                                        url: require("../../images/beowulfbank.png").default,
+                                        alt: "whey"
+                                    }
+                                }
+                            } />
+                    </div>
+                </div>
+                <div>
+                    <Link className="menu-item" to="/">О БРЕНДЕ</Link>
+                    <Link className="menu-item" to="/about">ГДЕ КУПИТЬ?</Link>
+                    <Link className="menu-item" to="/contact">ОПТОВЫМ КЛИЕНТАМ</Link>
+                    <Link className="menu-item" to="/contact">КОНТАКТЫ</Link>
+
+                </div>
             </Menu>)
     }
 
