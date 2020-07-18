d90acd2c711c673355dffa5571d4824552f2b25c
tranee23 <tranee95@gmail.com>
2020-07-19 01:15:29 +0800

    дроп

---
+++ src/components/burger/BurgerProductLine.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import Burger from "./Burger";
+import LineCard from "../dropDownMenu/js/LineCard.js";
+
+class BurgerProductLine extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="header__dropdown-content-product">
+                <h2 className="header__dropdown-content-title">Линейки</h2>
+                <div className="header__dropdown-content-product-card">
+                    <LineCard
+                        className={"header__dropdown-content-card"}
+                        content={
+                            {
+                                link: {
+                                    text: "Для насыщения организма витаминами",
+                                    url: "Card"
+                                },
+                                logo: {
+                                    url: require("../../images/logobasic.png").default,
+                                    alt: "logobasic"
+                                },
+                                product: {
+                                    url: require("../../images/whey.png").default,
+                                    alt: "whey"
+                                }
+                            }
+                        } />
+                    <LineCard
+                        className={"header__dropdown-content-card"}
+                        content={
+                            {
+                                link: {
+                                    text: "Для насыщения организма витаминами",
+                                    url: "Card"
+                                },
+                                logo: {
+                                    url: require("../../images/logoEssential.png").default,
+                                    alt: "logobasic"
+                                },
+                                product: {
+                                    url: require("../../images/Essentialbank.png").default,
+                                    alt: "whey"
+                                }
+                            }
+                        } />
+                </div>
+                <div className="header__dropdown-content-product-card">
+                    <LineCard
+                        className={"header__dropdown-content-card"}
+                        content={
+                            {
+                                link: {
+                                    text: "Для насыщения организма витаминами",
+                                    url: "Card"
+                                },
+                                logo: {
+                                    url: require("../../images/logoEssential.png").default,
+                                    alt: "logobasic"
+                                },
+                                product: {
+                                    url: require("../../images/premiumwhey.png").default,
+                                    alt: "whey"
+                                }
+                            }
+                        } />
+                    <LineCard
+                        className={"header__dropdown-content-card"}
+                        content={
+                            {
+                                link: {
+                                    text: "Для насыщения организма витаминами",
+                                    url: "Card"
+                                },
+                                logo: {
+                                    url: require("../../images/logoSuperior.png").default,
+                                    alt: "logobasic"
+                                },
+                                product: {
+                                    url: require("../../images/beowulfbank.png").default,
+                                    alt: "whey"
+                                }
+                            }
+                        } />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default BurgerProductLine;
