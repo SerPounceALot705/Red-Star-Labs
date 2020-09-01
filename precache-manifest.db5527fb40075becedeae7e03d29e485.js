290022b6d14703fb40292afca58255846c418a9c
Valerie Dmitryuk <cosmic.pony705@gmail.com>
2020-09-01 17:05:54 +0800

    Updates

---
+++ precache-manifest.db5527fb40075becedeae7e03d29e485.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d45d0709e61b55dfb1cf905332f42a0",
+    "url": "/Red-Star-Labs/index.html"
+  },
+  {
+    "revision": "5ed9118b16993a09cca0",
+    "url": "/Red-Star-Labs/static/css/2.e2f6717f.chunk.css"
+  },
+  {
+    "revision": "4655d6506980372c402e",
+    "url": "/Red-Star-Labs/static/css/main.c04ff1e7.chunk.css"
+  },
+  {
+    "revision": "5ed9118b16993a09cca0",
+    "url": "/Red-Star-Labs/static/js/2.b0356644.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Red-Star-Labs/static/js/2.b0356644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4655d6506980372c402e",
+    "url": "/Red-Star-Labs/static/js/main.d760337e.chunk.js"
+  },
+  {
+    "revision": "ea587d639d49c9eb405f",
+    "url": "/Red-Star-Labs/static/js/runtime-main.83e5e080.js"
+  },
+  {
+    "revision": "c2d031e50f36c12c1de5a4ea8f867174",
+    "url": "/Red-Star-Labs/static/media/Essentialbank.c2d031e5.png"
+  },
+  {
+    "revision": "5a0c49c8eb2cb5601886f27e71d2969b",
+    "url": "/Red-Star-Labs/static/media/TTFirsNeue-DemiBold.5a0c49c8.woff2"
+  },
+  {
+    "revision": "d83dbf77431a7a5e0dfbf9b95f567aab",
+    "url": "/Red-Star-Labs/static/media/TTFirsNeue-Medium.d83dbf77.woff2"
+  },
+  {
+    "revision": "42e4e35561d5fd7d32b793b20320bbe2",
+    "url": "/Red-Star-Labs/static/media/TTFirsNeue-Regular.42e4e355.woff2"
+  },
+  {
+    "revision": "cd628e7e7837a76f8d8b315e05ec3a92",
+    "url": "/Red-Star-Labs/static/media/about-brand.cd628e7e.png"
+  },
+  {
+    "revision": "962e399ff740770123b13e0dd09cc4f2",
+    "url": "/Red-Star-Labs/static/media/adaptive-photo-gal.962e399f.png"
+  },
+  {
+    "revision": "ab919c767050138d49246c850f668636",
+    "url": "/Red-Star-Labs/static/media/back-contacts.ab919c76.png"
+  },
+  {
+    "revision": "d885a75b1609dba8abb469d315afba55",
+    "url": "/Red-Star-Labs/static/media/back-customers.d885a75b.png"
+  },
+  {
+    "revision": "7f5cf95acdf1c7795e195316e6a003a7",
+    "url": "/Red-Star-Labs/static/media/back-obrende.7f5cf95a.png"
+  },
+  {
+    "revision": "6b00246ee1d62540ea4a4af6e51cfb55",
+    "url": "/Red-Star-Labs/static/media/back.6b00246e.png"
+  },
+  {
+    "revision": "7cf7fc9a4a24fedc7d32c3c8a69cbeca",
+    "url": "/Red-Star-Labs/static/media/banner.7cf7fc9a.png"
+  },
+  {
+    "revision": "2a9a8b9a3475ac5203d9a4b86107417f",
+    "url": "/Red-Star-Labs/static/media/beowulfbank.2a9a8b9a.png"
+  },
+  {
+    "revision": "d59f57e4a3ac0d4adf8910f321559fd3",
+    "url": "/Red-Star-Labs/static/media/card-product-fone.d59f57e4.png"
+  },
+  {
+    "revision": "a94fd3a39d956af49c808dd0f9dc41c4",
+    "url": "/Red-Star-Labs/static/media/contact-foto.a94fd3a3.png"
+  },
+  {
+    "revision": "09400134c0a50203fadf995606d2070b",
+    "url": "/Red-Star-Labs/static/media/fb.09400134.png"
+  },
+  {
+    "revision": "879c6ef5c27c99f891310967539eef35",
+    "url": "/Red-Star-Labs/static/media/figure.879c6ef5.png"
+  },
+  {
+    "revision": "b178cb4f29334d5009be1ca75e434b01",
+    "url": "/Red-Star-Labs/static/media/fullFon.b178cb4f.png"
+  },
+  {
+    "revision": "154c88ca06da984d29e9d90343c4fa46",
+    "url": "/Red-Star-Labs/static/media/gallery-item1.154c88ca.png"
+  },
+  {
+    "revision": "d70303e9147c5b3c6b3e61d66a3f6deb",
+    "url": "/Red-Star-Labs/static/media/gallery-item2.d70303e9.png"
+  },
+  {
+    "revision": "bbd4cb4ce5a5569a4792e008e5fd5409",
+    "url": "/Red-Star-Labs/static/media/gallery-item3.bbd4cb4c.png"
+  },
+  {
+    "revision": "c500634cc4adb54036c3586b877b5069",
+    "url": "/Red-Star-Labs/static/media/line-photo.c500634c.png"
+  },
+  {
+    "revision": "352cf5d3ab888568f9fe51e01a88404e",
+    "url": "/Red-Star-Labs/static/media/line-product-item.352cf5d3.png"
+  },
+  {
+    "revision": "c0a0212db1a1b123bf1c709840d7e3db",
+    "url": "/Red-Star-Labs/static/media/map.c0a0212d.png"
+  },
+  {
+    "revision": "0ca41b39f8ececcd01f1d5f5c3ca3e05",
+    "url": "/Red-Star-Labs/static/media/premiumWhey2.0ca41b39.png"
+  },
+  {
+    "revision": "843de47dacc4d174719944a94037e2a0",
+    "url": "/Red-Star-Labs/static/media/premiumwhey.843de47d.png"
+  },
+  {
+    "revision": "b14e5e9d7998e81afe03c8bbcb1c4163",
+    "url": "/Red-Star-Labs/static/media/product-photo.b14e5e9d.png"
+  },
+  {
+    "revision": "9070dd023b03df1f56c329f297a16291",
+    "url": "/Red-Star-Labs/static/media/product.9070dd02.png"
+  },
+  {
+    "revision": "88b4a1d944f78c1ada6c86719c020b65",
+    "url": "/Red-Star-Labs/static/media/productslide.88b4a1d9.png"
+  },
+  {
+    "revision": "445a2bad9d41a090e4659ed757a8673c",
+    "url": "/Red-Star-Labs/static/media/promo.445a2bad.png"
+  },
+  {
+    "revision": "b7232de1395520339ac22e69598d3a62",
+    "url": "/Red-Star-Labs/static/media/red-back.b7232de1.png"
+  },
+  {
+    "revision": "6ff1a056528de4b9390262cdf20886f6",
+    "url": "/Red-Star-Labs/static/media/red-star-labs-logo.6ff1a056.png"
+  },
+  {
+    "revision": "8a6f8ac3f149e1713c8be38f78463d53",
+    "url": "/Red-Star-Labs/static/media/slider.8a6f8ac3.png"
+  },
+  {
+    "revision": "b05852bb704001631858d95b99cd6bcb",
+    "url": "/Red-Star-Labs/static/media/tringle.b05852bb.png"
+  },
+  {
+    "revision": "c4fc432e51a708fe0f3319f21ee79580",
+    "url": "/Red-Star-Labs/static/media/whey.c4fc432e.png"
+  }
+]);
